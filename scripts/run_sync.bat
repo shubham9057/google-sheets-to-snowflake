f62e0508dc784c5bef9a4cdfe7ec5b28be4f0440
shubham-singhal-fb <105shubhamnks@gmail.com>
2025-07-21 13:16:34 +0530

    fix bat file

--- scripts/run_sync.bat
+++ scripts/run_sync.bat
@@ -1,6 +1,10 @@
 @echo off
+
 REM Change directory to project path
-cd /d C:\Users\Atrium\Documents\ThoughtSpot\google-sheet-to-snowflake\scripts
+cd /d C:\Users\Atrium\Documents\ThoughtSpot\google-sheet-to-snowflake
+
+REM Activate the virtual environment
+call venv\Scripts\activate
 
 REM Run the sync script
-python sync_google_sheet.py
+call python ./scripts/sync_google_sheet.py
